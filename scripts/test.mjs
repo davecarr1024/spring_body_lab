11280c9930d4ee8df885a7e12516ceee90d84c08
Dave Carr <davecarr1024@gmail.com>
2026-09-04 10:07:34 -0400

    Support Node 22 test discovery

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,4 +1,4 @@
-import { rm } from "node:fs/promises";
+import { readdir, rm } from "node:fs/promises";
 import { execFile } from "node:child_process";
 import { promisify } from "node:util";
 
@@ -7,7 +7,10 @@ const coverage = process.argv[2] === "coverage";
 
 await rm(".test-dist", { force: true, recursive: true });
 await run("node_modules/.bin/tsc", ["--project", "tsconfig.test.json"]);
-const args = [...(coverage ? ["--experimental-test-coverage"] : []), "--test", ".test-dist/tests"];
+const tests = (await readdir(".test-dist/tests", { recursive: true }))
+  .filter((file) => file.endsWith(".test.js"))
+  .map((file) => `.test-dist/tests/${file}`);
+const args = [...(coverage ? ["--experimental-test-coverage"] : []), "--test", ...tests];
 const { stdout, stderr } = await run(process.execPath, args);
 process.stdout.write(stdout);
 process.stderr.write(stderr);
